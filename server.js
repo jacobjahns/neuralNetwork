da33424201a1d6cd6f7caae0501f89c75e6e4f79
J4c0b <glue.crafter@gmail.com>
2021-05-27 16:23:41 +0200

    added node server for hosting && visual improvements

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+// Allow static paths in client folder
+app.use(express.static(__dirname));
+
+const PORT = 4321;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
